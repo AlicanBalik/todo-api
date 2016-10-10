f784a75821bb8a0ef13acfb337b4161831a99581
Alican Balik <alicanbalik@outlook.com>
2016-10-10 15:55:44 +0200

    added underscore examples

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 //  Example displaying Dynamic array with POST
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore'); // we are gonna use underscore for refactoring our get and post /todos
 
 var app = express(); // we call express variable as a function.
 var PORT = process.env.PORT || 1992;
@@ -25,6 +26,7 @@ var PORT = process.env.PORT || 1992;
 var todos = [];
 var todoNextId = 1; //for increasing id numbers.
 
+
 // anytime a json request comes in, express is gonna parse it and we are gonna be able to access it via request.body
 app.use(bodyParser.json());
 
@@ -40,17 +42,21 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     //we have to parse req.params.id to int because any request parameters are always STRING. In order to match it with an INT number, we have to convert it.
-    var todoId = parseInt(req.params.id, 10); //parseInt(string, base);
+    var todoId = parseInt(req.params.id, 10); //parseInt(string, radix);
+    // Specify 10 for the decimal numeral system commonly used by humans
     // Iterate over todos array. Find the match.
-    var matchedTodo;
 
-    todos.forEach(function (todo) {
-        //todoId was string, todo.id was int. We converted todoId to INT above.
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-
+    // var matchedTodo;
+    // todos.forEach(function (todo) {
+    //     //todoId was string, todo.id was int. We converted todoId to INT above.
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
+    //ustteki kodu kullanmak yerine underscore kutuphanesinden _.findWhere fonksiyonunu kullanabiliriz.
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    //_.findwhere is a function from underscore library.
+    //_.findWhere(list, properties)
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -62,11 +68,22 @@ app.get('/todos/:id', function (req, res) {
 // POST /todos
 // use new npm dependency called body-parser - npm install body-parser@1.13.3 --save
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    // var body = req.body;
+    var body = _.pick(req.body,'description','completed');
+    // _.pick(object, keys) - senin belirledigin key leri kaydediyor.
+    // sadece description&completed keylerini storelamak istiyoruz. Baska bir key daha yazilirsa, onu kaydetmez.
+    // Bir nevi filtering.
+    //Id kismini yazmamiza gerek yok cunku kod kendisi id belirliyor assagida. Kullanicidan almiyor degeri.
+
+    //this if statement is useful for validating the objects.
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();                                      //trim removes before and after a string
+    } // 400 = badRequest
 
     //add id field
     body.id = todoNextId++;
-    body.test = 'testing id:';
+    //set body.description to trimmed body.description in order to get rid of the spaces.
+    body.description = body.description.trim();
 
     //push body into array
     todos.push(body);
